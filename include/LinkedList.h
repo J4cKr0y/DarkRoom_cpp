6909ed5da0ecf1798689ba1dc36e3948edb1c4bc
Jacky Royer <kiky270281@gmail.com>
2025-10-13 23:41:05 +0200

    feat: LinkedList

---
+++ include/LinkedList.h
@@ -0,0 +1,17 @@
+﻿#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+// node def
+typedef struct Node {
+    int data;
+    struct Node* next;
+} Node;
+
+// main functions
+void addElement(Node** headRef, int newData);
+void deleteElement(Node** headRef, int key);
+void printList(Node* node);
+int searchElement(Node* head, int key);
+void freeList(Node** headRef); 
+
+#endif
