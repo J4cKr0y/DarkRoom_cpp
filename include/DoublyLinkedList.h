7bb05d4c2d6c487dcb5dc8d526b695beed4d2ef3
Jacky Royer <kiky270281@gmail.com>
2025-10-18 13:42:11 +0200

    feat: DoublyLinkedList

---
+++ include/DoublyLinkedList.h
@@ -0,0 +1,18 @@
+﻿#ifndef DOUBLYLINKEDLIST_H
+#define DOUBLYLINKEDLIST_H
+
+// Définition du nœud
+typedef struct DNode {
+    int data;
+    struct DNode* prev;
+    struct DNode* next;
+} DNode;
+
+// Fonctions principales
+void addElementD(DNode** headRef, int newData);
+void deleteElementD(DNode** headRef, int key);
+void printListD(DNode* node);
+int searchElementD(DNode* head, int key);
+void freeListD(DNode** headRef); // Fonction pour libérer la mémoire
+
+#endif
