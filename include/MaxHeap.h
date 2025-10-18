05b2ea4a61286a0feec1880e7201b42b0c087bea
Jacky Royer <kiky270281@gmail.com>
2025-10-18 13:46:23 +0200

    feat: add Tas-Heap

---
+++ include/MaxHeap.h
@@ -0,0 +1,19 @@
+﻿#ifndef MAXHEAP_H
+#define MAXHEAP_H
+
+// Définition de la structure de Tas
+typedef struct MaxHeap {
+    int* arr;
+    int size;
+    int capacity;
+} MaxHeap;
+
+// Fonctions principales
+MaxHeap* createMaxHeap(int capacity);
+void insertElement(MaxHeap* heap, int element);
+int extractMax(MaxHeap* heap);
+void heapify(MaxHeap* heap, int index);
+void printHeap(MaxHeap* heap);
+void freeHeap(MaxHeap* heap); // Fonction pour libérer la mémoire
+
+#endif
